6a25c19927c0f48cec6ae55bcb3a291eeeb5632c
Navinda Perera <navindaperera99@gmail.com>
2019-10-20 00:46:19 +0530

    added comments to functions

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Work
 This is my work projects
 This project consist of php files
+added comments to functions
--- basic.php
+++ basic.php
@@ -8,18 +8,22 @@
         return $n;
     }
 
+    // add function
     function add($a,$b) {
         return $a+$b;
     }
 
+    // subtract function
     function subtract($a,$b) {
         return $a-$b;
     }
 
+    // multiply function
     function multiply($a,$b) {
         return $a*$b;
     }
 
+    // divide function
     function divide($a,$b) {
         return $a/$b;
     }
