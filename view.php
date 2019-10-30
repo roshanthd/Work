794c79365d7efb751c7cd086e3ef7c2fb32c06d9
Kusal Kahaduwa <kusalhasintha2012@gmail.com>
2019-10-30 14:02:48 +0530

    Create new view file

---
+++ view.php
@@ -0,0 +1,22 @@
+<?php
+    $name = "HelloWorld";
+    $name1 = "hiiii"
+    echo($name);
+    echo($name1);
+    function name($n) {
+        return $n;
+    }
+    // multiply function
+    function multiply($a,$b) {
+        return $a*$b;
+    }
+    // divide function
+    function divide($a,$b) {
+        return $a/$b;
+    }
+    
+    //mod function
+    function mod($a,$b){
+        return $a%$b
+    }
+?>
